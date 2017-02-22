b0d4307d09971d13e50dcab009d53f840a82dbad
lei <dolphinzheng@dolphinzheng.com>
2017-02-22 22:14:21 +0800

    add model & view file

---
+++ daigou.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields
+
+
+
+class Client(models.Model):
+
+    _name = 'daigou.client'
+
+    _rec_name = 'weixinname'
+
+    name = fields.Char('Name', required=True)
+
+    weixinname = fields.Char('Wei Xin ID')  
+
+    telephone = fields.Char('number of telephone', required=True)
+
+    adresse = fields.Char('adresse of client', required=True)
+
+    city = fields.Char('The city', required=True)
+
+    cp = fields.Char('Code postal')
+
+    province = fields.Char('The province')
+
+    country = fields.Char('The country')
+
