262576ade6d32f7490ab662a86165244d4e1b73d
lei <dolphinzheng@dolphinzheng.com>
2017-02-22 22:12:04 +0800

    add openerp.py'

---
+++ __openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+
+    'name': 'Daigou - module for tiesanjiao',
+
+    'version': '0.1',
+
+    'category': 'Generic Modules/Others',
+
+    'description': """ This is one module for Odoo v9
+
+    """,
+
+    'author': 'Dolphin',
+
+    'depends': ['base'],
+
+    'installable': True,
+
+    'auto_install': False,
+
+}
