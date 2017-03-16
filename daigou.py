efde557bf7be2e2098af260254c79bd3ebab4163
benoir126 <dolphinzheng@hotmail.com>
2017-03-16 17:44:15 +0100

    modif view : add menuitem

--- daigou.py
+++ daigou.py
@@ -54,10 +54,10 @@ class ProductCategory(models.Model):
 
     name = fields.Char(u'产品类别名称', required=True)
     complete_name = fields.Char(u'产品类别描述', required=True)
-    parent_id = fields.Many2one('daigou.product.category','Parent Category', select=True, ondelete='cascade')
-    child_id = fields.One2many('daigou.product.category', 'parent_id', string='Child Categories')
-    sequence = fields.Integer('Sequence', select=True, help="Gives the sequence order when displaying a list of product categories.")
-    type = fields.Selection([('view','View'), ('normal','Normal')], 'Category Type', help="A category of the view type is a virtual category that can be used as the parent of another category to create a hierarchical structure.")
+    #parent_id = fields.Many2one('daigou.product.category','Parent Category', select=True, ondelete='cascade')
+    #child_id = fields.One2many('daigou.product.category', 'parent_id', string='Child Categories')
+    #sequence = fields.Integer('Sequence', select=True, help="Gives the sequence order when displaying a list of product categories.")
+    #type = fields.Selection([('view','View'), ('normal','Normal')], 'Category Type', help="A category of the view type is a virtual category that can be used as the parent of another category to create a hierarchical structure.")
 
 class Product(models.Model):
 
@@ -67,10 +67,10 @@ class Product(models.Model):
 
     name = fields.Char(u'产品名称', required=True)
     complete_name = fields.Char(u'产品描述', required=True)
-    parent_id = fields.Many2one('daigou.product.category', required=True, index=True,store=True)
-    sequence = fields.Integer('Sequence', select=True, help="Gives the sequence order when displaying a list of product categories.")
-    price_item_vente_unit_str = fields.Float(u'商品最初定价')
-    price_item_vente_unit_achat = fields.Float(u'商品购买价')
+    #parent_id = fields.Many2one('daigou.product.category', required=True, index=True,store=True)
+    #sequence = fields.Integer('Sequence', select=True, help="Gives the sequence order when displaying a list of product categories.")
+    #price_item_vente_unit_str = fields.Float(u'商品最初定价')
+    #price_item_vente_unit_achat = fields.Float(u'商品购买价')
 
 #----------------------------------------------------------
 # Orders : every order for one client
